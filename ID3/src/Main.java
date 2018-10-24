28add5bb95ef630a366bfdfdfff406e3c04e7b6b
Ocnarf2070 <francogonzalezsanchez@alu.uma.es>
2018-10-24 18:03:13 +0200

    Creación de rama

--- ID3/src/Main.java
+++ ID3/src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("holo");
+		System.out.println("Esto es de Franco");
 	}
 
 }
